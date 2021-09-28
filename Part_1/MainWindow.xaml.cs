49a720548316f57158f22481f9b9df46889819b6
RossanaPalma <70126235+RossanaPalma@users.noreply.github.com>
2021-09-27 22:10:47 -0700

    Add project files.

---
+++ Part_1/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿/// Chapter No. 11 - Homework5_Part_1
+/// File Name: homework5.java
+/// @author: Rossana Palma
+/// Date:  September 25, 2021
+///
+/// Problem Statement: Write a program that calculates a person’s Body Mass Index (BMI) and displays a message 
+/// about the user's health range. Use a GUI for the input and output.
+///  
+/// 
+/// Overall Plan:
+/// 1) Declare variables
+/// 2) Convert inputs to double
+/// 3) Calculate BMI and convert to string
+/// 4) Use statement if else to identify and brush the calculated health range
+/// 5) Display results on the screen
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Variables
+            double weight;
+            double height;
+            double bmi;
+            //Convert value into double
+            weight = double.Parse(weightTextBox.Text);
+            height = double.Parse(heightTextBox.Text);
+            bmi = (weight * 720) / (height * height);
+            //Convert bmi to string and assign it to resultTextBox
+            resultTextBox.Text = bmi.ToString();
+            if(bmi > 26) // above : yellow
+            {
+                bmiCanvas.Background = Brushes.Yellow;
+                messageTextBox.Text = ("You are above the health range");
+            }
+            else if(bmi >= 15 && bmi <=26) // healthy : green
+            {
+                bmiCanvas.Background = Brushes.Green;
+                messageTextBox.Text = ("You are healthy");
+            }
+            else if(bmi < 15)
+            {
+                bmiCanvas.Background = Brushes.Blue;
+                messageTextBox.Text = ("You are under the health range");
+            }
+        }
+    }
+}
---
+++ Part_2/Program.cs
@@ -0,0 +1,59 @@
+﻿/// Chapter No. 11 - Homework5_Part_2
+/// File Name: homework5.java
+/// @author: Rossana Palma
+/// Date:  September 27, 2021
+///
+/// Problem Statement: Write a program that will print out statistics for eight coin tosses. The user will 
+/// input either an “h” for heads or a “t” for tails for the eight tosses and the program displays he total 
+/// number and percentages of heads and tails. 
+/// 
+/// Overall Plan:
+/// 1) Declare and initialize variables
+/// 2) Prompt user for h or t
+/// 3) 
+/// 4) 
+/// 5) 
+
+using System;
+
+namespace Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and initialize variables    
+            int h = 0, t = 0, count =0;
+            double headTotal=0, tailTotal=0;
+            int toss=0;
+
+            Console.WriteLine("Enter h for head or t for tail. When finish type -1:");
+            
+            while (toss != -1)
+               {
+               toss = int.Parse(Console.ReadLine());
+                  if (toss == 0)
+                     {
+                        headTotal++; // 0:head
+                     }
+                  else
+                    {
+                        tailTotal++; //1:tail
+                    }
+
+               }
+            
+            //Convert value into double        
+            h = Convert.ToDouble(headTotal) / 8 * 100;
+            t = Convert.ToDouble(tailTotal) / 8 * 100;
+            
+
+            Console.WriteLine("Percent of heads:" + h);
+            Console.WriteLine("Percent of tails:" + t);
+            Console.WriteLine("Total of heads:" + headTotal);
+            Console.WriteLine("Total of tails:" + tailTotal);
+
+        }
+    }
+}
+   
